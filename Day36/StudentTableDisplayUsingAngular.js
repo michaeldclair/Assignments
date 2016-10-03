8463717961adbcd0eea063d54f8dc5fa55b4a3fd
Michael Clair <michaeldclair@gmail.com>
2016-10-03 16:39:19 -0400

    Commit of Day36

---
+++ Day36/StudentTableDisplayUsingAngular.js
@@ -0,0 +1,43 @@
+angular.module("StudentMod", [])
+	.controller("StudentCtrl", [function() {
+		var self = this;
+		self.about = "About AngularJS";
+
+
+
+		self.refresh = function() {
+			for (var std of self.students) {
+				std.vis = true;
+			}
+		};
+		self.hide = function(studentId) {
+			self.students[studentId].vis = false;
+
+		};
+
+		self.satcheck = function() {
+		var minimumSat = document.getElementById('satbox').value;
+			for (var std of self.students) {
+					std.vis = true;
+				if (std.sat < minimumSat) {
+					std.vis = false;
+				}
+			}
+
+		};
+
+		self.students = [
+		{"id":100,"firstname":"Eric","lastname":"Ephram","gpa":3.0,"sat":1200,"majorid":1,"vis":true},
+		{"id":110,"firstname":"Greg","lastname":"Gould","gpa":2.5,"sat":1100,"majorid":null,"vis":true},
+		{"id":120,"firstname":"Adam","lastname":"Ant","gpa":3.2,"sat":1300,"majorid":null,"vis":true},
+		{"id":130,"firstname":"Howard","lastname":"Hess","gpa":3.7,"sat":1600,"majorid":4,"vis":true},
+		{"id":140,"firstname":"Charles","lastname":"Caldwell","gpa":2.1,"sat":900,"majorid":null,"vis":true},
+		{"id":150,"firstname":"James","lastname":"Joyce","gpa":2.5,"sat":1100,"majorid":null,"vis":true},
+		{"id":160,"firstname":"Doug","lastname":"Dumas","gpa":3.1,"sat":1350,"majorid":2,"vis":true},
+		{"id":170,"firstname":"Kevin","lastname":"Kraft","gpa":2.7,"sat":1000,"majorid":null,"vis":true},
+		{"id":180,"firstname":"Frank","lastname":"Fountain","gpa":2.5,"sat":1000,"majorid":null,"vis":true},
+		{"id":190,"firstname":"Brian","lastname":"Biggs","gpa":2.3,"sat":950,"majorid":null,"vis":true}
+		]
+
+	}]); // end controller
+	
